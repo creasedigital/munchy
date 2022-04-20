a208065125ba249b721e789bbe6ae2134539e41e
creasedigital <ojorechris@gmail.com>
2022-04-20 20:03:58 +0100

    side drawer backdrop added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import { Box } from '@chakra-ui/react';
 import Auxilliary from '../../hoc/Auxilliary';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => {
+	const [showSideDrawer, setShowSideDrawer] = useState(false);
+	const sideDrawerClosedHandler = () => {
+		setShowSideDrawer(!showSideDrawer);
+	};
+
 	return (
 		<Auxilliary>
 			<Toolbar />
-			<SideDrawer />
+			<SideDrawer open={showSideDrawer} closed={sideDrawerClosedHandler} />
 			<Box as='main' marginTop='72px'>
 				{props.children}
 			</Box>
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,41 +1,47 @@
 import { Box } from '@chakra-ui/react';
+import Auxilliary from '../../../hoc/Auxilliary';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../Toolbar/NavigationItems/NavigationItems';
+import Backdrop from '../../Backdrop/Backdrop';
 
 const SideDrawer = (props) => {
 	return (
-		<Box
-			pos='fixed'
-			w='280px'
-			maxWidth='70%'
-			h='100%'
-			left='0'
-			top='0'
-			zIndex='200'
-			bgColor='white'
-			color='munchy.main'
-			p='32px 16px'
-			boxSizing='border-box'
-			transition='transform 0.3s ease-out'
-			sx={{
-				'@media (min-width: 500px)': {
-					display: 'none',
-				},
-				'&.open': {
-					transform: 'translateX(0)',
-				},
-				'&.close': {
-					transform: 'translateX(100%)',
-				},
-			}}
-		>
-			<Box mb={8} w='50%'>
-				<Logo height='11%' />
+		<Auxilliary>
+			<Backdrop show={props.open} clicked={props.closed} />
+			<Box
+				ClassName={props.open ? 'open' : 'closed'}
+				pos='fixed'
+				w='280px'
+				maxWidth='70%'
+				h='100%'
+				left='0'
+				top='0'
+				zIndex='200'
+				bgColor='white'
+				color='munchy.main'
+				p='32px 16px'
+				boxSizing='border-box'
+				transition='transform 0.3s ease-out'
+				css={{
+					'@media (min-width: 500px)': {
+						display: 'none',
+					},
+					'&.open': {
+						transform: 'translateX(0)',
+					},
+					'&.close': {
+						transform: 'translateX(-100%)',
+					},
+				}}
+			>
+				<Box mb={8} w='50%'>
+					<Logo height='11%' />
+				</Box>
+				<Box as='nav'>
+					<NavigationItems />
+				</Box>
 			</Box>
-			<Box as='nav'>
-				<NavigationItems />
-			</Box>
-		</Box>
+		</Auxilliary>
 	);
 };
 export default SideDrawer;
