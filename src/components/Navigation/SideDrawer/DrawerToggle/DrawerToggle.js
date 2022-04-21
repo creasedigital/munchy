d6f2b7a3d2209007f5e17cf4d31740eaf926ebf8
creasedigital <ojorechris@gmail.com>
2022-04-21 13:45:28 +0100

    hamburger icon added

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,6 +1,28 @@
 import { Box } from '@chakra-ui/react';
 
 const DrawerToggle = (props) => {
-	return <Box onClick={props.clicked}>MENU</Box>;
+	return (
+		<Box
+			onClick={props.clicked}
+			width='40px'
+			height='100%'
+			display='flex'
+			flexFlow='column'
+			justifyContent='space-around'
+			alignItems='center'
+			padding='10px 0'
+			boxSizing='border-box'
+			cursor='pointer'
+			sx={{
+				'@media (min-width: 500px)': {
+					display: 'none',
+				},
+			}}
+		>
+			<Box width='90%' height='3px' backgroundColor='white'></Box>
+			<Box width='90%' height='3px' backgroundColor='white'></Box>
+			<Box width='90%' height='3px' backgroundColor='white'></Box>
+		</Box>
+	);
 };
 export default DrawerToggle;
